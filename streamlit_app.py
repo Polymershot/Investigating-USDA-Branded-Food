b231ff2efdee9c8ac15653e280c138010f6bde96
Polymershot <69413289+Polymershot@users.noreply.github.com>
2024-07-17 19:09:24 -0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,115 @@
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+
+
+
+#Dataset Page
+
+# <https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/>
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]):
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    #default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+
+#turn object dtype to cateogory for the function
+score_data = pd.read_csv('C:/Users/rdn91/OneDrive/Desktop/Coding/Python_Projects/usda_branded_foods/data/transformed_data/score_data.csv')
+obj_cols = score_data.select_dtypes(include="object").columns
+score_data[obj_cols] = score_data[obj_cols].astype("category")
+
+
+
+#Title 
+st.title("USDA Branded Food Dataset")
+
+#Body Text
+st.write("Filter by food type by choosing the 'branded_food_category' column. The scores are all the way to the right:)")
+
+#Read the dataframe
+st.dataframe(filter_dataframe(score_data))
+
+
+#Graph Page
+st.title("Visualize the Scores")
+user_column = st.multiselect("Filter dataframe on", score_data['branded_food_category'].unique(), max_selections=1)
+score_data = score_data[score_data['branded_food_category'].isin(user_column)]
+food_plot = sns.kdeplot(score_data, x = 'minmaxscaler_score')
+st.markdown("Number of unique food items: " + str(score_data['description'].nunique()))
+st.pyplot(food_plot.get_figure())
